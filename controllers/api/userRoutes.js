e24fb849491f67897adfd58f3f2ecf5c18f46140
Sarah Turner <turnersarah228@gmail.com>
2022-05-04 21:13:32 -0500

    added packages to homeRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
+const sequelize = require('../../config/connections');
 const db = require('../../models');
 
 // GET all drivers
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connections');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  // store: new SequelizeStore({
+  //   db: sequelize,
+  // }),
+};
+
+app.use(session(sess));
+
 const hbs = exphbs.create({});
+// const hbs = exphbs.create({ });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -20,5 +34,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
 });
