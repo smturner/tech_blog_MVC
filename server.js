ce8b36e6027db4f78d96c9247b380fc64d06b5d6
Sarah Turner <turnersarah228@gmail.com>
2022-05-06 18:58:39 -0500

    added form on handlebars and homeroute in index.js

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const path = require('path');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connections');
 
-const PORT = process.env.PORT || 3001;
+
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  // don't need for project
+  // store: new SequelizeStore({
+  //   db: sequelize
+  // })
+};
+app.use(session(sess));
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
