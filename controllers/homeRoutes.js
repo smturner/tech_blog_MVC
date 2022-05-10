528f62fc6368d0a629a9da801e8559e11c34a1ad
Sarah Turner <turnersarah228@gmail.com>
2022-05-09 22:09:35 -0500

    trying to get heroku to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,46 +3,46 @@ const { User, Post } = require('../models');
 
 
 // route that renders the homepage blogposts
-router.get('/', async (req, res) => {
-    try{
-        const dashboardData = await Post.findAll({
-          include: [
-            {
-              model: User,
-              attributes:['name'],
-            },
-          ],
+// router.get('/', async (req, res) => {
+//     try{
+//         const dashboardData = await Post.findAll({
+//           include: [
+//             {
+//               model: User,
+//               attributes:['name'],
+//             },
+//           ],
            
-        });
-        const posts = dashboardData.map((post) => post.get({plain:true}))
+//         });
+//         const posts = dashboardData.map((post) => post.get({plain:true}))
 
-        res.render('homepage',
-        {
-          posts,
-          logged_in:req.session.logged_in
-        }
-        );
-        // res.status(200).json(dashboardData);
-    }catch (err) {
-        res.status(500).json(err);
-    }
-});
+//         res.render('homepage',
+//         {
+//           posts,
+//           logged_in:req.session.logged_in
+//         }
+//         );
+//         // res.status(200).json(dashboardData);
+//     }catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
-router.get('/signup', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/dashboard');
-    return;
-  }
-    res.render('signup')
-})
+// router.get('/signup', (req, res) => {
+//   if (req.session.logged_in) {
+//     res.redirect('/dashboard');
+//     return;
+//   }
+//     res.render('signup')
+// })
 
-router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/dashboard');
-        return;
-    }
-    res.render('login')
-});
+// router.get('/login', (req, res) => {
+//     if (req.session.logged_in) {
+//         res.redirect('/dashboard');
+//         return;
+//     }
+//     res.render('login')
+// });
 
 module.exports=router
 
