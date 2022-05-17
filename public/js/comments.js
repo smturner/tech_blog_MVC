1a234fcc98eac2b29f8ef95c37211c4e4b63e0f7
Sarah Turner <turnersarah228@gmail.com>
2022-05-16 22:48:54 -0500

    added changes

--- public/js/comments.js
+++ public/js/comments.js
@@ -2,10 +2,12 @@ const commentForm = async(e) => {
     console.log("hello")
     e.preventDefault()
 
-    const comment_content= document.querySelector('#commentBody').value.trim()
+    const comment_content= document.getElementById('commentBody').value.trim()
+    console.log(comment_content)
     const post_id=  window.location.toString().split("/")[
         window.location.toString().split("/").length - 1
       ];
+      console.log(post_id)
 
 if (comment_content) {
     const response = await fetch('/api/comments', {
@@ -14,7 +16,9 @@ if (comment_content) {
         headers: { 'Content-Type': 'application/json'},
     });
     if (response.ok) {
-        document.location.reload()
+        console.log('TEST')
+        // document.location.reload('/dashboard')
+       
     }else {
         alert(response.statusText)
     }
