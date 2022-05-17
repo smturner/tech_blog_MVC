168b5577ef3d204d12fc33c49817964dc6601320
Sarah Turner <turnersarah228@gmail.com>
2022-05-16 20:05:26 -0500

    working on comments

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const helpers = require('./utils/helpers');
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const hbs = exphbs.create({ helpers });
 
